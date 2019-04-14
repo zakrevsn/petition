d93dcbbb7c33dfb097a6d03ab029a2ad099d8885
Natalia Zakrevskaya <zakrevsn@gmail.com>
2019-04-14 23:12:10 +0200

    Phonefriendly and touch events

--- public/script.js
+++ public/script.js
@@ -27,6 +27,43 @@ canvas.addEventListener("mousemove", function(evt) {
     var dataURL = canvas.toDataURL();
     signature.value = dataURL;
 });
+canvas.addEventListener("touchstart", function(evt) {
+    evt.preventDefault();
+    var touches = evt.changedTouches;
+    // from https://stackoverflow.com/a/18794913
+    var viewportOffset = canvas.getBoundingClientRect();
+    var top = viewportOffset.top;
+    var left = viewportOffset.left;
+    clientX = touches[0].clientX - left;
+    clientY = touches[0].clientY - top;
+});
+canvas.addEventListener("touchend touchcancel", function(evt) {
+    evt.preventDefault();
+    clientX = undefined;
+    clientY = undefined;
+});
+canvas.addEventListener("touchmove", function(evt) {
+    evt.preventDefault();
+    var touches = evt.changedTouches;
+    // from https://stackoverflow.com/a/18794913
+    var viewportOffset = canvas.getBoundingClientRect();
+    var top = viewportOffset.top;
+    var left = viewportOffset.left;
+    if (clientX == undefined && clientY == undefined) {
+        return;
+    }
+    ctx.beginPath();
+    ctx.moveTo(clientX, clientY);
+    ctx.strokeStyle = "green";
+    ctx.lineTo(touches[0].clientX - left, touches[0].clientY - top);
+    ctx.lineWidth = 3;
+    ctx.stroke();
+    clientX = touches[0].clientX - left;
+    clientY = touches[0].clientY - top;
+    var dataURL = canvas.toDataURL();
+    signature.value = dataURL;
+});
+
 if (signature.value) {
     // from https://stackoverflow.com/a/4776378
     var img = new Image();
